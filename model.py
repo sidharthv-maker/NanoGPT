5ee90fe4a7df481a1fc5d0947ccf0af1d1014246
sidharthv-maker <siddharth08vutukuru@gmail.com>
2026-05-25 23:12:47 +0530

    Add the final model class

--- model.py
+++ model.py
@@ -20,17 +20,15 @@ class MHSelfAttention(nn.Module):
         Q = self.WQ(x)
         K = self.WK(x)
         V = self.WV(x)
-
         Q.view(B, T, self.head_num, self.head_dim).transpose(1,2)
         K.view(B, T, self.head_num, self.head_dim).transpose(1,2)
         V.view(B, T, self.head_num, self.head_dim).transpose(1,2)
-
         scores = Q @ K.transpose(-2,-1)
         score = score/(self.emb_dim ** 0.5)
         scores = scores.masked_fill(self.mask[:T, :T] == 0, float("-inf"))
         weights = F.softmax(score, -1)
-        out = weights @ V
 
+        out = weights @ V
         out = out.transpose(1,2).contiguous()
         out = out.view(B, T, self.emb_dim)
         out = self.W_O(out)
@@ -51,11 +49,34 @@ class Transformer(nn.Module):
        out = self.attn(x)
        x = x + out
        x = self.norm1(x)
-       
        out = self.ff(x)
        x = x+out
        x = self.norm2(x)
        return x
+
+class TinyGPT(nn.Module):
+    def __init__(self, emb_dim, head_num, seq_len):
+        super().__init__()
+        self.emb_dim = emb_dim
+        self.head_num = head_num
+        self.head_dim = emb_dim//head_num
+
+        self.emb = nn.Embedding(28, emb_dim)
+        self.posemb = nn.Embedding(seq_len, emb_dim)
+        self.mod = nn.ModuleList([Transformer(emb_dim, head_num, seq_len) for _ in range(20)])
+        self.norm = nn.LayerNorm(emb_dim)
+        self.lin = nn.Linear(emb_dim, 28)
+
+    def forward(self, x):
+        tok = self.emb(x)
+        _ , T, _ = tok.shape
+        pos = self.posemb(torch.arange(T))
+        x = tok + pos  
+        for block in self.mod:
+            x = block(x)
+        x = self.norm(x)
+        out = self.lin(x)
+        return out
 
 with open("shakespeare.txt", "r") as f:
     text = f.read()
